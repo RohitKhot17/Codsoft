c2fb6e4ae335a2129219b25ce488377d120e126c
RohitKhot17 <rohitkhot2000@gmail.com>
2023-12-23 10:54:04 +0530

    Task2 is Done

---
+++ Task2.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Input: Take marks obtained in each subject
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scan.nextInt();
+
+        int[] marks = new int[numSubjects];
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + ": ");
+            marks[i] = scan.nextInt();
+        }
+
+        int totalMarks = calculateTotalMarks(marks);
+
+        double averagePercentage = calculateAveragePercentage(totalMarks, numSubjects);
+
+        char grade = calculateGrade(averagePercentage);
+
+        displayResults(totalMarks, averagePercentage, grade);
+        scan.close();
+    }
+
+    private static int calculateTotalMarks(int[] marks) {
+        int totalMarks = 0;
+        for (int mark : marks) {
+            totalMarks += mark;
+        }
+        return totalMarks;
+    }
+
+    private static double calculateAveragePercentage(int totalMarks, int numSubjects) {
+        return (double) totalMarks / numSubjects;
+    }
+
+    private static char calculateGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return 'A';
+        } else if (averagePercentage >= 80) {
+            return 'B';
+        } else if (averagePercentage >= 70) {
+            return 'C';
+        } else if (averagePercentage >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+
+    private static void displayResults(int totalMarks, double averagePercentage, char grade) {
+        System.out.println("\nResults:");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+    }
+}
